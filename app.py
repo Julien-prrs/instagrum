ab4b8cbc436a835b1eaed01b9ad7918bddd1b972
Julien-prrs <perros.julien@outlook.fr>
2020-02-18 11:03:13 +0100

    Organise folder tree

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 # App
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config["MONGO_URI"] = "mongodb://localhost:27017/pythonFlaskMMO"
 mongo = PyMongo(app)
 
