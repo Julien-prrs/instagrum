421cb4590310a94ea43d0eb80cda333458cd0953
Julien-prrs <perros.julien@outlook.fr>
2020-02-18 15:18:06 +0100

    Sync

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 # App
-app = Flask(__name__, static_folder="static", template_folder="templates")
-app.config["MONGO_URI"] = "mongodb://localhost:27017/pythonFlaskMMO"
-mongo = PyMongo(app)
+app = Flask(__name__, static_folder="static", template_folder="templates");
+app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskInstagram";
+mongo = PyMongo(app);
 
 # Routes
 @app.route('/')
-def index():
-    return '<h1>Page - Index</h1>'
+def home():
+    users = mongo.db.users.find();
+    return render_template('layouts/home.html', title="Accueil", users=users);
